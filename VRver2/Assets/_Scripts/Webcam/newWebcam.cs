464c1e01a0b7b4bea9f30ce7ec39abe9c5f96331
meangpu <51825872+meangpu@users.noreply.github.com>
2021-12-03 03:35:16 +0700

    finish camera

--- VRver2/Assets/_Scripts/Webcam/newWebcam.cs
+++ VRver2/Assets/_Scripts/Webcam/newWebcam.cs
@@ -6,15 +6,17 @@ using UnityEngine.UI;
 public class newWebcam : MonoBehaviour
 {
     public RawImage rawImage;
+    [SerializeField] Material colorMat;
 
     void Start()
     {
-        // openCam();
+        openCam();
     }
 
     public void openCam()
     {
         WebCamTexture webcamtex = new WebCamTexture();
+        colorMat.SetTexture("_MainTex", webcamtex);
         rawImage.texture = webcamtex;
         rawImage.material.mainTexture = webcamtex;
         webcamtex.Play();
