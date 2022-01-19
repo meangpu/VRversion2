1dedeea3c56f93125115e0bcdedb5a0399e6e51e
meangpu <51825872+meangpu@users.noreply.github.com>
2022-01-19 21:26:41 +0700

    aa

--- VRver2/Assets/__Scripts/Mainmenu/SettingSlider.cs
+++ VRver2/Assets/__Scripts/Mainmenu/SettingSlider.cs
@@ -15,6 +15,13 @@ public class SettingSlider : MonoBehaviour
 
     [Header("Volumn")]
     [SerializeField] TMP_Text masterVolText;
+    [SerializeField] Slider masterVolSlider;
+    
+    [SerializeField] TMP_Text sfxVolText;
+    [SerializeField] Slider sfxVolSlider;
+
+    [SerializeField] TMP_Text musicVolText;
+    [SerializeField] Slider musicVolSlider;
 
     private void Start() 
     {
@@ -22,12 +29,28 @@ public class SettingSlider : MonoBehaviour
         {
             eyeText.SetText(v.ToString("0.00"));
         });
-        
+
         tableSlider.onValueChanged.AddListener((v) => 
         {
             tableText.SetText(v.ToString("0.00"));
         });
 
+        // volumn
+
+        masterVolSlider.onValueChanged.AddListener((v) => 
+        {
+            masterVolText.SetText(v.ToString("0.00"));
+        });
+
+        sfxVolSlider.onValueChanged.AddListener((v) => 
+        {
+            sfxVolText.SetText(v.ToString("0.00"));
+        });
+
+        musicVolSlider.onValueChanged.AddListener((v) => 
+        {
+            musicVolText.SetText(v.ToString("0.00"));
+        });
 
 
 
