1bf2bbeaedf8b6e48f6a71b4193654c82f02aca8
meangpu <51825872+meangpu@users.noreply.github.com>
2022-01-25 18:12:39 +0700

    Create colliderCut.cs

---
+++ VRver2/Assets/__Scripts/colliderCut.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using Obi;
+
+public class colliderCut : MonoBehaviour
+{
+ 	ObiSolver solver;
+    [SerializeField] string tagName = "kill";
+ 
+	void Awake(){
+		solver = GetComponent<ObiSolver>();
+	}
+
+	void OnEnable () {
+		solver.OnCollision += Solver_OnCollision;
+	}
+
+	void OnDisable(){
+		solver.OnCollision -= Solver_OnCollision;
+	}
+
+	void Solver_OnCollision (object sender, Obi.ObiSolver.ObiCollisionEventArgs e)
+	{
+		var world = ObiColliderWorld.GetInstance();
+
+		// just iterate over all contacts in the current frame:
+		foreach (Oni.Contact contact in e.contacts)
+		{
+			if (contact.distance < 0.01)
+			{
+				ObiColliderBase col = world.colliderHandles[contact.bodyB].owner;
+				if (col != null)
+				{
+                    if (col.gameObject.tag == tagName)
+                    {
+                        int particleIndex = solver.simplices[contact.bodyA];
+                        ObiSolver.ParticleInActor pa = solver.particleToActor[particleIndex];
+                        ObiRope nowRope = pa.actor as ObiRope;  // get to rope script
+                        meCutRope(nowRope, pa.indexInActor);  // get into actual index of rope
+                    }
+				}
+			}
+		}
+	}
+
+    void meCutRope(ObiRope _rope, int _numIndex)
+    {
+        // prevent error out of list!!
+        if(_rope.elements.Count > _numIndex)
+        {
+            _rope.Tear(_rope.elements[_numIndex]);
+            _rope.RebuildConstraintsFromElements();
+        }
+    }
+
+
+
+
+
+
+
+
+}
