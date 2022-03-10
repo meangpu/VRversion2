af5aa1dbb9f1d241140f8efb9616f05ba99afc4b
meangpu <51825872+meangpu@users.noreply.github.com>
2022-03-10 22:18:47 +0700

    w

--- VRver2/Assets/__Scripts/Laparoscopy/toolResetPosition.cs
+++ VRver2/Assets/__Scripts/Laparoscopy/toolResetPosition.cs
@@ -7,6 +7,7 @@ public class toolResetPosition : MonoBehaviour
 
     public void resetPosAndRot()
     {
+        Debug.Log("weee");
         transform.position = position;
         transform.rotation = Quaternion.Euler(rotation);
     }
