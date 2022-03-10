679ffe718fe324252a93d45ad1f07b2fcc752f94
meangpu <51825872+meangpu@users.noreply.github.com>
2022-03-11 00:24:11 +0700

    morse

--- VRver2/Assets/__Scripts/BombRelated/MorseManager.cs
+++ VRver2/Assets/__Scripts/BombRelated/MorseManager.cs
@@ -1,22 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MorseManager : MonoBehaviour
 {
+    [Header("Setting")]
     [SerializeField] float timeNextLoop = 1;
     [SerializeField] float timeInLoop = .2f;
+    [SerializeField] float timeShort = .3f;
+    [SerializeField] float timeLong = .6f;
 
-    [SerializeField] GameObject lightObject;
+    [SerializeField] MeshRenderer lightObject;
     [SerializeField] AudioSource shortSound;
     [SerializeField] AudioSource longSound;
-    
 
-    void Start()
+    [SerializeField] Material matLightOn;
+    [SerializeField] Material matLightOff;
+
+    [Header("main")]
+    [SerializeField] string mainMorseWord;
+    private IEnumerator coroutine;
+
+    private void Start() 
     {
+        coroutine = doStartMorse();
+        StartCoroutine(coroutine);
+    }
+
+    public IEnumerator doStartMorse()
+    {
+        foreach(char c in mainMorseWord)
+        {
+            if(c.ToString() == ".")
+            {
+                shortSound.Play();
+                turnLightOn();
+                yield return new WaitForSeconds(timeShort);
+                turnLightOff();
+            }
+            else if (c.ToString() == "-")
+            {
+                longSound.Play();
+                turnLightOn();
+                yield return new WaitForSeconds(timeLong);
+                turnLightOff();
+            }
+            yield return new WaitForSeconds(timeInLoop);
+        }
+
+        yield return new WaitForSeconds(timeNextLoop);
+        StartCoroutine(doStartMorse());
         
     }
 
-    void Update()
+    void turnLightOn()
     {
-        
+        lightObject.material = matLightOn;
     }
+
+    void turnLightOff()
+    {
+        lightObject.material = matLightOff;
+    }
+
 }
--- VRver2/Assets/__Scripts/ENV/BombGlowAndSound.cs
+++ VRver2/Assets/__Scripts/ENV/BombGlowAndSound.cs
@@ -49,9 +49,6 @@ public class BombGlowAndSound : MonoBehaviour
                 isRed = false;
                 meshRender.material = redGlow;
             }
-
-
-
         }
     }
 
