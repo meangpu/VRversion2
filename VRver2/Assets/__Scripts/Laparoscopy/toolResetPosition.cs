58e72888958ec3435f814313b6cccb0ba4f15e06
meangpu <51825872+meangpu@users.noreply.github.com>
2022-03-10 22:47:08 +0700

    Update toolResetPosition.cs

--- VRver2/Assets/__Scripts/Laparoscopy/toolResetPosition.cs
+++ VRver2/Assets/__Scripts/Laparoscopy/toolResetPosition.cs
@@ -4,9 +4,18 @@ public class toolResetPosition : MonoBehaviour
 {
     [SerializeField] Vector3 position;
     [SerializeField] Vector3 rotation;
+    Rigidbody rb;
+
+    private void Start() {
+        rb = transform.GetComponent<Rigidbody>();
+    }
+
+
 
     public void resetPosAndRot()
     {
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
         transform.position = position;
         transform.rotation = Quaternion.Euler(rotation);
     }
