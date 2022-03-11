6fc7fdd25e8d3eb08751c9b63a63649d6475d78f
meangpu <51825872+meangpu@users.noreply.github.com>
2022-03-11 14:38:42 +0700

    wa

---
+++ VRver2/Assets/__Scripts/setBossPosition.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setBossPosition : MonoBehaviour
+{
+    [SerializeField] Vector3 posValue = new Vector3(-0.0205061883f,1.4633683f,1.21283424f);
+    [SerializeField] Vector3 rotEul;
+   
+    [ContextMenu("aasdsad")]
+    public void DoSetPP() 
+    {
+        transform.position = posValue;
+        transform.rotation = Quaternion.Euler(rotEul);;
+
+    }
+}
